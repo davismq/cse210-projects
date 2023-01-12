09182058836a4d37af9ca8a9894009e186bc3893
davismq <davisquist@gmail.com>
2023-01-11 20:49:41 -0700

    Finished prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What was your score out of 100 for this class?");
+        string score = Console.ReadLine();
+
+        int numberScore = int.Parse(score);
+
+        if (numberScore >= 90)
+        {
+            Console.WriteLine("You got an A in the class.");
+        }
+        else if (numberScore >= 80)
+        {
+            Console.WriteLine("You got a B in the class.");
+        }
+        else if (numberScore >= 70)
+        {
+            Console.WriteLine("You got a C in the class.");
+        }
+        else if (numberScore >= 60)
+        {
+            Console.WriteLine("You got a D in the class.");
+        }
+        else
+        {
+            Console.WriteLine("You got an F in the class.");
+        }
     }
 }
