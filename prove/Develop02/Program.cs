01cbc288c156d13743c279b8035b39bbf5f83ba4
davismq <davisquist@gmail.com>
2023-01-14 23:07:39 -0700

    Tried to figure out journal thing

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.IO;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Console.WriteLine("Welcome to the Journal Program!");
+
+        int input = 0;
+
+        while (input != 5)
+        {
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+            input = int.Parse(Console.ReadLine());
+        }
     }
 }
